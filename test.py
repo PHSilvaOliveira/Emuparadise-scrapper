e8f63c59edcfd9dafae8c9f5aebca6d12304e1cf
PHSilvaOliveira <57330747+PHSilvaOliveira@users.noreply.github.com>
2020-07-21 20:28:32 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import webbrowser
 from tkinter import *
 import googlesearch
 
+#pyinstaller --onefile test.py
 def Baixar():
-    jogo = googlesearch.lucky(str(nome.get()) + 'emuparadise',tld='me')
+    jogo = googlesearch.lucky('emuparadise.me' + str(nome.get()),tld='me')
     print(jogo)
-    emuParadise = regex.compile(r'(https://www.emuparadise.me/)' + '([\w-]+)/' + '([\w-()]+/)' + '(\d+)')
+    emuParadise = regex.compile(r'(https://www.emuparadise.me/)' + '([\w-]+)/' + '([\w-()\[\]]+/)' + '(\d+)')
     mb = emuParadise.search(str(jogo))
     numero = mb.group(4)
-
     output = 'https://www.emuparadise.me/roms/get-download.php?gid=' + numero + '&test=true'
     webbrowser.open(str(output))
 
@@ -33,10 +33,3 @@ Salvarlinha.grid(row = 250, column = 50)
 #Propriedades da janela
 interface.geometry('400x100+0+0')
 interface.mainloop()
-
-emuParadise = regex.compile(r'(https://www.emuparadise.me/)' + '([\w-]+)/' + '([\w-()]+/)' + '(\d+)')
-mb = emuParadise.search(str(jogo))
-numero = mb.group(4)
-
-output = 'https://www.emuparadise.me/roms/get-download.php?gid=' + numero + '&test=true'
-webbrowser.open(str(output))
