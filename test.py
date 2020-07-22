c52ca92594b73547ab22d7189e14702060700859
PHSilvaOliveira <57330747+PHSilvaOliveira@users.noreply.github.com>
2020-07-21 21:31:03 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import googlesearch
 def Baixar():
     jogo = googlesearch.lucky('emuparadise.me' + str(nome.get()),tld='me')
     print(jogo)
-    emuParadise = regex.compile(r'(https://www.emuparadise.me/)' + '([\w-]+)/' + '([\w-()\[\]]+/)' + '(\d+)')
+    emuParadise = regex.compile(r'(https://www.emuparadise.me/)' + '([\w-()]+)/' + '([\w-()\[\]]+/)' + '(\d+)')
     mb = emuParadise.search(str(jogo))
     numero = mb.group(4)
     output = 'https://www.emuparadise.me/roms/get-download.php?gid=' + numero + '&test=true'
